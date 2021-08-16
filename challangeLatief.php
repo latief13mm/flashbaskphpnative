f80f1e418634e5e8ee34292dbc8abf067c51ae4c
latief13mm <41255506+latief13mm@users.noreply.github.com>
2021-08-16 13:11:52 +0700

    Saya membuat challangeLatief.php
    Membuat contoh tipe data dari 
    1. Integer 
    2. Float
    3. Boolean

---
+++ challangeLatief.php
@@ -0,0 +1,24 @@
+//Untuk Integer
+<?php 
+    $bil_satu = 12; 
+    $bil_kedua = 78; 
+    //Kedua variabel di atas merupakan variabel yang bertipe data integer. 
+?>
+
+//Untuk Float
+<?php 
+    $angka = 12.177; 
+    //variabel di atas adalah variabel yang bertipe data float karena berisi bilangan desimal. 
+?>
+
+//Untuk Boolean
+<?php
+	$x = true;
+	$y = false;
+	
+	echo $x; //Hasil: 1
+	echo "<br>";
+	echo $y; //Tidak ada hasil yang ditampilkan
+?>
+
+
